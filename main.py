d476bb5416b0cdec1653db2445da67bff328dfb2
Mr. Coding is EZ <153249227+GBCDguy@users.noreply.github.com>
2024-09-17 19:11:05 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+"""Defining python function:
+        https://docs.python.org/3/library/functions.html"""
+
+"""def my_function():
+        # Do this
+        # Then do this
+        # Finally do this"""
+
+
+def my_function():
+    print('hello')
+    print('bye')
+
+
+my_function()
+
+"""The rest is in karel the robot"""
